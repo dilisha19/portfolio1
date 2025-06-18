a5506a6982bbf19b093f1da9a754d63d91405184
Dilisha <152714010+dilisha19@users.noreply.github.com>
2025-06-18 23:02:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
